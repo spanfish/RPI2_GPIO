9d65c423457ab83d8f024c2bf5982814705f3cdc
spanfish <wang_xiangwei@hotmail.com>
2016-05-10 22:20:40 +0900

    initialize

---
+++ RPI/KERN_SRC/.RPI.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/spanfish/work/RPI2_GPIO/RPI/KERN_SRC/RPI.ko := /home/spanfish/raspberrypi/tools/arm-bcm2708/arm-bcm2708-linux-gnueabi/bin/arm-bcm2708-linux-gnueabi-ld -EL -r  -T ./scripts/module-common.lds --build-id  -o /home/spanfish/work/RPI2_GPIO/RPI/KERN_SRC/RPI.ko /home/spanfish/work/RPI2_GPIO/RPI/KERN_SRC/RPI.o /home/spanfish/work/RPI2_GPIO/RPI/KERN_SRC/RPI.mod.o
---
+++ RPI/KERN_SRC/.tmp_versions/RPI.mod
@@ -0,0 +1,2 @@
+/home/spanfish/work/RPI2_GPIO/RPI/KERN_SRC/RPI.ko
+/home/spanfish/work/RPI2_GPIO/RPI/KERN_SRC/RPI.o
